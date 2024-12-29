dbc0227ec19a6919d42555bf23868a2ddf4830b3
grzechuzz <grzegorz.rzepiela@student.pk.edu.pl>
2024-12-29 15:36:32 +0100

    Pliki helper.c, helper.h - dodano funkcje do tworzenia struktur IPC w celu ulepszenia czytelnosci kodu

--- helper.c
+++ helper.c
@@ -19,6 +19,62 @@ Dish menu[10] = {
 	{"Pizza Vegie Supreme", 44.99}
 };
 
+
+int create_sem(key_t key) {
+	int sem_id = semget(key, 1, IPC_CREAT|0644);
+	if (sem_id == -1) {
+		perror("blad w semget()");
+		exit(1);
+	}
+
+	if (semctl(sem_id, SEM_MUTEX_TABLES_DATA, SETVAL, 1) == -1) {
+		perror("blad w semctl() [inicjalizacja semaforow]");
+		exit(1);
+	}
+	
+	return sem_id;
+}
+
+int create_shm(key_t key, size_t size) {
+	int shm_id = shmget(key, size, IPC_CREAT|0644);
+	if (shm_id == -1) {
+		perror("blad w shmget()");
+	}
+
+	return shm_id;
+}
+
+int create_msg(key_t key) {
+	int msg_id = msgget(key, IPC_CREAT|0644);
+	if (msg_id == -1) {
+		perror("blad w msgget()");
+		exit(1);
+	}
+
+	return msg_id;
+}
+
+void remove_sem(int sem_id) {
+	if (semctl(sem_id, 0, IPC_RMID) == -1) {
+		perror("blad w semctl() [usuwanie semaforow]");
+	}
+}
+
+void remove_shm(int shm_id, void* addr) {
+	if (shmdt(addr) == -1) {
+		perror("blad w shmdt()");
+	}
+
+	if (shmctl(shm_id, IPC_RMID, NULL) == -1) {
+		perror("blad w shmctl() [usuwanie pamieci dzielonej]");
+	}
+}
+
+void remove_msg(int msg_id) {
+	if (msgctl(msg_id, IPC_RMID, NULL) == -1)
+		perror("blad w msgctl() [usuwanie kolejki komunikatow]");
+}
+
 void P(int sem_id, int sem_num) {
 	struct sembuf sbuf;
 	sbuf.sem_num = sem_num;
--- helper.h
+++ helper.h
@@ -41,6 +41,12 @@ typedef struct {
 
 extern Dish menu[10];
 
+int create_sem(key_t key, int val);
+int create_shm(key_t key, size_t size);
+int create_msg(key_t key);
+void remove_sem(int sem_id);
+void remove_shm(int shm_id, void* addr);
+void remove_msg(int msg_id);
 void P(int sem_id, int sem_num);
 void V(int sem_id, int sem_num);
 void print_single_order(int id);
