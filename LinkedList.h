6e8d1682f0a474cb0bf2dff41ef44b8a5fef70c6
grzechuzz <grzegorz.rzepiela@student.pk.edu.pl>
2025-01-10 15:18:41 +0100

    Dodano header dla listy wiazanej

---
+++ LinkedList.h
@@ -0,0 +1,24 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+#include "helper.h"
+
+typedef struct {
+	Client client;
+	Node* next;	
+} Node;
+
+typedef struct {
+	Node* head;
+	int size;
+	int current_size;
+} LinkedList;
+
+void initialize_node(Client c);
+void initialize_linked_list(LinkedList* ll);
+int get_current_size(const LinkedList* ll);
+void add(LinkedList* ll, Client c);
+Client remove(const LinkedList* ll, int group_size);
+void display(LinkedList* ll);
+
+#endif // LINKEDLIST_H
+
